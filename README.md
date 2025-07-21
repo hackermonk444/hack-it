b9955c461d441ee4d5e89f728d2cccb7d3eb55bf
hackermonk444 <hackermonk444@gmail.com>
2025-07-22 04:04:01 +0530

    testing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hack-it
-My hacking journey repo !!! 
+<h3>My hacking journey repo !!!<h3>
+<h3>repo started 22nd July 2025 <h3> 
+<h3> official journey started 1st July 2025<h3>
---
+++ python/01_test.py
@@ -0,0 +1,7 @@
+def test():
+    print ("TESTING and setting up git repo  ")
+    print("following PYTHON FOR HACKERS 101 !!!")
+
+test()
+name = "nimish jindal"
+startDate="22-07-2025"
